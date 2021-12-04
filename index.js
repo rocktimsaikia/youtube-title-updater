8b957fedb5376ea8790252445d089cc8573f537a
Rocktim Saikia <rocktimthedev@gmail.com>
2021-12-04 14:36:41 +0530

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const { google } = require('googleapis');
+const VIDEO_ID = 'I4wRB7NujEY';
+
+const updateDescription = async () => {
+  // Init auth
+  const authClient = new google.auth.OAuth2({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  authClient.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+  // Get video details
+  const youtube = google.youtube({
+    auth: authClient,
+    version: 'v3',
+  });
+  const response = await youtube.videos.list({
+    id: VIDEO_ID,
+    part: 'statistics,snippet',
+  });
+
+  const { snippet } = response.data.items[0];
+  const { title, description } = snippet;
+
+  // update the description version number
+  const updatedDescVersion = parseInt(description.match(/\d+/g)) + 1;
+  const updatedDesc = description.replace(/\d+/g, updatedDescVersion);
+
+  // update title of the video with a new date
+  const today = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+  const updatedTitle = `Today is ${today}`;
+
+  const updatedResponse = await youtube.videos.update({
+    part: 'snippet',
+    requestBody: {
+      id: VIDEO_ID,
+      snippet: { ...snippet, description: updatedDesc },
+    },
+  });
+
+  console.log(updatedResponse.status);
+};
+
+updateDescription();
